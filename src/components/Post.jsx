722c763eb0c8d4af17f20f7188648fabe6e936b7
Zenky <bzenky@gmail.com>
2022-07-05 21:44:04 -0300

    validação formulário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -28,7 +28,12 @@ export function Post({ author, content, publishedAt }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('')
         setNewCommentText(event.target.value)
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Este campo é obrigatório')
     }
 
     function deleteComment(commentToDelete) {
@@ -38,6 +43,8 @@ export function Post({ author, content, publishedAt }) {
 
         setComments(commentsWithoutDeletedOne)
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0
 
     return (
         <article className={styles.post}>
@@ -73,10 +80,17 @@ export function Post({ author, content, publishedAt }) {
                     value={newCommentText}
                     placeholder="Deixe um comentário"
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button
+                        type="submit"
+                        disabled={isNewCommentEmpty}
+                    >
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
