926b3d6e7d411514f86f8a9fdcb1f27d25fc3644
Zenky <bzenky@gmail.com>
2022-07-06 20:43:07 -0300

    closures

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -8,7 +8,9 @@ export function Comment({ content, onDeleteComment }) {
     const [likeCount, setLikeCount] = useState(0)
 
     function handleLikeComment() {
-        setLikeCount(likeCount + 1)
+        setLikeCount((state) => {
+            return state + 1
+        })
     }
 
     function handleDeleteComment() {
